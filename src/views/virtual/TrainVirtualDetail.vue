28a65362ab19c3726e6820a3d487e920dc6b932c
leo <ingrogger@gmail.com>
2019-12-02 18:04:39 -0300

    corregir detalle virtual

--- src/views/virtual/TrainVirtualDetail.vue
+++ src/views/virtual/TrainVirtualDetail.vue
@@ -7,7 +7,7 @@
       </v-list-item-icon>
       <v-list-item-content>
         <v-list-item-title class="white--text">
-          Detalle entrenamiento virtual
+          Detalle entrenamiento
         </v-list-item-title>
       </v-list-item-content>
     </v-list-item>
